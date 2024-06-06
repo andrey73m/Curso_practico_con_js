6bd0443e2d07655a7eddf2c5dd906f9ffb3a5214
andrey73m <andrey12mora@gmail.com>
2024-06-06 12:49:45 -0500

    Agregando funcionalidades de botones

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const menuEmail =  document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 const iconMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const iconCartMenu = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
 
 
 const toggleDesktopMenu = () => {
@@ -9,8 +11,24 @@ const toggleDesktopMenu = () => {
 }
 
 const toggleMobileMenu = () => {
+  const isProductDetailClosed = productDetail.classList.contains('inactive');
+
+  if(!isProductDetailClosed)
+  productDetail.classList.add('inactive')
+
   mobileMenu.classList.toggle('inactive');
+  
+}
+
+const toggleProductDetail = () => {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if(!isMobileMenuClosed)
+  mobileMenu.classList.add('inactive')
+
+  productDetail.classList.toggle('inactive')
 }
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 iconMenu.addEventListener('click', toggleMobileMenu)
+iconCartMenu.addEventListener('click', toggleProductDetail)
