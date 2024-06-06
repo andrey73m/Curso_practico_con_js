1a11f3525988fc4a00a3df735ffc32141748549b
andrey73m <andrey12mora@gmail.com>
2024-06-06 11:23:36 -0500

    Implementando menu en escritorio y movil

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const menuEmail =  document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
-const body = document.querySelector('body')
+const iconMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 const toggleDesktopMenu = () => {
   desktopMenu.classList.toggle('inactive');
 }
 
+const toggleMobileMenu = () => {
+  mobileMenu.classList.toggle('inactive');
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
+iconMenu.addEventListener('click', toggleMobileMenu)
