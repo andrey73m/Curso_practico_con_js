b102d15b114dc9b899d8d49e6a0afc14d38d55c3
andrey73m <andrey12mora@gmail.com>
2024-06-06 11:04:47 -0500

    Funcionando CSS de componentes y agregando funcionalidades

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Ay tu marido Belcast")
+const menuEmail =  document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu')
+const body = document.querySelector('body')
+
+const toggleDesktopMenu = () => {
+  desktopMenu.classList.toggle('inactive');
+}
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
